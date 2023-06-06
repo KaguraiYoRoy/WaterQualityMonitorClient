79a571302e4ef4f18c50a2c4bbc78043b7dedacc
iYoRoy <3363384229@qq.com>
2023-06-06 18:16:47 +0800

    Add signal handler

--- monitord/BlockQueue.h
+++ monitord/BlockQueue.h
@@ -5,17 +5,43 @@
 template <typename T>
 class BlockQueue
 {
+public:
+	BlockQueue(){}
+
+	~BlockQueue(){}
+
+	void Put(const T& x)
+	{
+		std::unique_lock<std::mutex> guard(m_mutex);
+		m_queue.push_back(x);
+		m_cond.notify_all();
+	}
+
+	void Put(T&&x)
+	{
+		std::unique_lock<std::mutex> guard(m_mutex);
+		m_queue.push_back(std::move(x));
+		m_cond.notify_all();
+	}
+
+	T Take()
+	{
+		std::unique_lock<std::mutex> guard(m_mutex);
+		while (m_queue.size() == 0)
+			m_cond.wait(guard);
+		T front(std::move(m_queue.front()));
+		m_queue.pop_front();
+		return std::move(front);
+	}
+
+	size_t Size()
+	{
+		std::unique_lock<std::mutex> guard(m_mutex);
+		return m_queue.size();
+	}
+
 private:
 	mutable std::mutex m_mutex;
 	std::condition_variable m_cond;
 	std::list<T>     m_queue;
-public:
-	BlockQueue();
-	~BlockQueue();
-	void Put(const T& x);
-	void Put(T&& x);
-	T Take();
-	size_t Size();
-
 };
-
--- monitord/main.cpp
+++ monitord/main.cpp
@@ -6,34 +6,64 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <pthread.h>
+#include <signal.h>
 
 #include "Log.h"
+#include "BlockQueue.h"
 
 Log Logger;
 
+bool isExit;
+
+static void SigHandler(int sig);
 void* CronTimer(void*);
 void* UploadTimer(void*);
 void* CommandProcesser(void*);
 
 int main() {
     Logger.OpenFile("monitor.log");
-    Logger.WriteLog(INFO, "Water Quality MonitorDaemon Starting...");
+    Logger.WriteLog(INFO, "Water Quality MonitorDaemon starting...");
 
-    
+    signal(SIGINT, SigHandler);
+    isExit = false;
+
+    while (!isExit);
+
+    Logger.WriteLog(INFO, "Program exiting. Please wait...");
 
     Logger.Close();
     return 0;
 }
 
-void* CronTimer(void*) {
+static void SigHandler(int sig) {
+    switch (sig)
+    {
+    case SIGINT: {
+        isExit = true;
+        break;
+    }
+    default:
+        break;
+    }
+}
 
+void* CronTimer(void*) {
+    while (!isExit) {
+
+    }
     return 0;
 }
 
 void* UploadTimer(void*) {
+    while (!isExit) {
+
+    }
     return 0;
 }
 
 void* CommandProcesser(void*) {
+    while (!isExit) {
+
+    }
     return 0;
 }
