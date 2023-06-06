04824f64c49e677a625e57adaa5e6514a3a7e232
iYoRoy <3363384229@qq.com>
2023-06-06 21:16:30 +0800

    Configuration file parsing

--- monitord/main.cpp
+++ monitord/main.cpp
@@ -8,14 +8,16 @@
 
 #include <wiringPi.h>
 #include <wiringSerial.h>
+#include <curl/curl.h>
 #include <json/json.h>
 
 #include "Log.h"
 #include "BlockQueue.h"
+#include "clipp.h"
 
 Log Logger;
 pthread_t tCronTimer, tUploadTimer, tCommandProcesser;
-
+std::string Configfile, Logfile;
 bool isExit;
 
 static void SigHandler(int sig);
@@ -23,8 +25,39 @@ void* CronTimer(void*);
 void* UploadTimer(void*);
 void* CommandProcesser(void*);
 
-int main() {
-    Logger.OpenFile("monitor.log");
+int main(int argc, char* argv[]) {
+    Logfile = "";
+
+    auto cli = (
+        clipp::value("Config file", Configfile),
+        clipp::option("-l", "--log") & clipp::value("log file", Logfile)
+        );
+
+    if (!clipp::parse(argc, argv, cli)) {
+        std::cout << clipp::make_man_page(cli, argv[0]);
+        return 0;
+    }
+
+    std::ifstream ConfigReaderStream;
+    ConfigReaderStream.open(Configfile);
+    Json::Reader JsonReader;
+    Json::Value JsonConfigRoot;
+    if (!JsonReader.parse(ConfigReaderStream, JsonConfigRoot)) {
+        std::cout << "FATAL: Failed to load Config file!" << std::endl;
+        return 0;
+    }
+        
+    if (Logfile == (std::string)"") {
+        if (JsonConfigRoot.isMember("LogFile")) {
+            Logfile = JsonConfigRoot["LogFile"].asString();
+        }
+        else {
+            std::cout << "FATAL: \"LogFile\" doesn't set in Config File!" << std::endl;
+            return 0;
+        }
+    }
+
+    Logger.OpenFile(Logfile);
     Logger.WriteLog(INFO, "Water Quality MonitorDaemon starting...");
 
     signal(SIGINT, SigHandler);
